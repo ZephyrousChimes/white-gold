8c5c43a8e80ce83bae9adf2bf84ec5d243eda681
KushalSuvan <kushalsuvanjenamani@gmail.com>
2025-07-18 02:52:59 +0530

    fix: invalid post URL handled

--- src/lib/mdxUtils.ts
+++ src/lib/mdxUtils.ts
@@ -4,7 +4,11 @@ import { parseMDX } from './mdxParser';
 
 export async function getSerializedMdxForSlug(slug: string) {
   const mdxPath = path.resolve(process.cwd(), `src/app/posts/${slug}/content.mdx`);
-  
-  const mdxContent = await readFile(mdxPath, 'utf8');
-  return await parseMDX(mdxContent);
+
+  try {
+    const mdxContent = await readFile(mdxPath, 'utf8');
+    return await parseMDX(mdxContent);
+  } catch {
+    return { mdxSource: null } ;
+  }
 }
